cdec0c069277ce453e598432e2695a7afef9c4a4
andb3 <andb3003@yahoo.com>
2022-02-09 12:12:59 -0500

    added search, expand/collapse all

--- assets/js/ztips.js
+++ assets/js/ztips.js
@@ -1,15 +1,49 @@
-function toggle_tip(date) {
-    tip_id = '#tip-'+date.split('/').join('')
-    console.log("toggling " + tip_id)
-    document.querySelector(tip_id).classList.toggle("expanded")
-    document.querySelector(`${tip_id} > .ztip-text`).classList.toggle("expanded")
-    document.querySelector(`${tip_id} > .ztip-expand`).innerHTML = ""
+function filter_tips(term) {
+    document.querySelectorAll(".ztip").forEach((el) => {
+        let searchable_text = [el.querySelector(".ztip-title").innerHTML, el.querySelector(".ztip-text").innerHTML]
+        let has_term = searchable_text.some((t) => t.toLowerCase().includes(term.toLowerCase()))
+        el.style.display = (has_term) ? 'flex' : 'none'
+    })
+}
 
+function toggle_all() {
+    let all_tips = Array.from(document.querySelectorAll(".ztip"))
+    if (all_tips.every((el) => el.classList.contains("expanded"))) {
+        all_tips.forEach((el) => toggle_tip(el.id, false))
+    } else {
+        all_tips.forEach((el) => toggle_tip(el.id, true))
+    }
+}
+
+function update_toggle_all_button() {
+    let all_tips = Array.from(document.querySelectorAll(".ztip"))
+    if (all_tips.every((el) => el.classList.contains("expanded"))) {
+        document.querySelector(".ztip-expand-all").innerHTML = "compress"
+    } else {
+        document.querySelector(".ztip-expand-all").innerHTML = "expand"
+    }
+}
+
+function toggle_tip(tip_id, force) {
+    document.querySelector(`#${tip_id}`).classList.toggle("expanded", force)
+    document.querySelector(`#${tip_id} > .ztip-text`).classList.toggle("expanded", force)
+    var icon;
+    switch (force) {
+        case true: icon = "expand_less"; break;
+        case false: icon = "expand_more"; break;
+        default:
+            let now_expanded = document.querySelector(`#${tip_id}`).classList.contains("expanded")
+            icon = (now_expanded) ? "expand_less" : "expand_more"
+            break;
+    }
+    document.querySelector(`#${tip_id} > .ztip-header > .ztip-expand`).innerHTML = icon
+    update_toggle_all_button()
 }
 
 function tip_component(tip) {
+    let tip_id = 'tip-' + tip['date'].split('/').join('')
     return `
-    <div class="ztip" id="tip-${tip['date'].split('/').join('')}" onclick="toggle_tip('${tip['date']}')">
+    <div class="ztip" id="${tip_id}" onclick="toggle_tip('${tip_id}')">
         <div class="ztip-header">
             <span class="ztip-title">${tip['title']}</span>
             <span class="ztip-date">${tip['date']}</span>
@@ -21,14 +55,13 @@ function tip_component(tip) {
 }
 
 function populate_html(zips_dict) {
-    for(var i = 0; i < 142; i++) {
+    for (var i = 0; i < 142; i++) {
         let tip = zips_dict[i]
         $('#ztips-container').append(tip_component(tip))
     }
 }
 
 $(document).ready(function () {
-
     $.get('ztips.txt', function (data) {
         var zips_dict = {}
         data = data.split("\n");
@@ -44,5 +77,4 @@ $(document).ready(function () {
         console.log(zips_dict);
         populate_html(zips_dict);
     });
-
 });
