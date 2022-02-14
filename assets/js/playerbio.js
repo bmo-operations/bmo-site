4b9a648079a272801e96429ff23f715fecb65cb6
andb3 <andb3003@yahoo.com>
2022-02-14 13:45:47 -0500

    fixed roster layout, updated styling on cards

--- assets/js/playerbio.js
+++ assets/js/playerbio.js
@@ -1,16 +1,20 @@
-
-function getJson(player) {
+function getAllBios() {
    var request = new XMLHttpRequest();
    request.open("GET", "assets/bios.json", false);
    request.send(null)
-   return JSON.parse(request.responseText)[player];
+   return JSON.parse(request.responseText)
+}
+
+function getPlayerBio(player) {
+   let allBios = getAllBios()
+   return allBios[player]
 }
 
 // When the user clicks on the button, open the modal
 function onbtnclick(modalid, player) {
    // parse relevant data
-   var info = getJson(player);
-
+   var info = getPlayerBio(player);
+   console.log(`player bio = ${info}`)
    // set text
    document.getElementById("height").innerText = "Height: " + info.height;
    document.getElementById("hometown").innerText = "Hometown: " + info.hometown;
@@ -28,3 +32,42 @@ function onbtnclick(modalid, player) {
 function onspanclick(modalid) {
    document.getElementById(modalid).style.display = "none";
 }
+
+function populateHTML() {
+   console.log("populating html")
+   let bios = Object.values(getAllBios())
+   console.log(`bios = ${bios}`)
+   let parser = new DOMParser()
+   bios.forEach(bio => {
+      let card = `
+      <div class="roster-card">
+         <img class="player-image" src="${bio.img}" alt="${bio.name}" />
+         <div class="player-container">
+            <div class="player-text">
+               <p class="player-name">${bio.name}${(bio.captain) ? " (captain)" : ""}</p>
+               <p class="player-nickname-container"><span class="player-aka">aka</span> <span
+                     class="player-nickname">${bio.nickname}</span></p>
+            </div>
+            <div class="player-info">
+               <div class="player-info-item">
+                  <span class="material-icons-outlined">school</span>
+                  <p class="player-info-item-text">Class of ${bio.class}</p>
+               </div>
+               <div class="player-info-item">
+                  <span class="material-icons-outlined">home_work</span>
+                  <p class="player-info-item-text">${bio.hometown}</p>
+               </div>
+            </div>
+            <button class="button" onclick="onbtnclick('popup', '${bio.id}')">More</button>
+         </div>
+      </div>
+      `
+      console.log(`bio html = ${parser.parseFromString(card, "text/html")}`)
+      document.querySelector(".roster-grid").appendChild(parser.parseFromString(card, "text/html").body)
+   });
+}
+
+window.onload = function() { 
+   console.log("window loaded")
+   populateHTML()
+ }
