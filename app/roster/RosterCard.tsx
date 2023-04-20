5582c319bf0e2899fc66cdad1b4badec1556a092
CalNightingale <cal.nightingale@gmail.com>
2023-04-20 13:47:13 -0400

    no base paths for optimized images

--- app/roster/RosterCard.tsx
+++ app/roster/RosterCard.tsx
@@ -11,7 +11,7 @@ export default function RosterCard({ player, year, onMore }: { player: Player, y
     return (
         <RosterCardBase align="stretch">
             <Image
-                src={`${process.env.NEXT_PUBLIC_BASE_PATH}/content/roster/${year}/${player.id}.jpg`}
+                src={`/content/roster/${year}/${player.id}.jpg`}
                 alt={`Picture of ${player.name}`}
                 sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
                 width={0}
